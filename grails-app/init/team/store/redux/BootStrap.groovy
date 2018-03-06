473a4fe861c1d3580e3c7518a7c76ab346660c5e
geekman9097 <powerplanets@aol.com>
2018-03-06 09:34:59 -0500

    Update Bootstrap to hold a product order by default

--- grails-app/init/team/store/redux/BootStrap.groovy
+++ grails-app/init/team/store/redux/BootStrap.groovy
@@ -5,7 +5,9 @@ class BootStrap {
     def init = { servletContext ->
         def cat1 = new Category(name:'Category One').save(failOnError:true)
         def mfg1 = new Manufacturer(name:'Manny One').save(failOnError:true)
-        new Product(category:cat1,manufacturer:mfg1,name:'Prod 1', description:'Described',price:1.0,rating:'5',score:1).save(failOnError:true)
+        def prod1 = new Product(category:cat1,manufacturer:mfg1,name:'Prod 1', description:'Described',price:1.0,rating:'5',score:1).save(failOnError:true)
+
+        new ProductOrder(number:'1',product:prod1,quantity:2).save(failOnError:true);
     }
     def destroy = {
     }
